ebaff3d0ba0826833fe26d6d07197ced46f5f551
Benjamin Hindman <benjamin.hindman@gmail.com>
2021-12-20 15:52:14 -0800

    Refactor redundant shell code into functions

--- check_style.sh
+++ check_style.sh
@@ -1,63 +1,111 @@
 #!/bin/bash
 
-# This export is for the terminal's font.
-export TERM=xterm-color
+# Helper that returns 0 if the version in the first argument is
+# greater than or equal to the version in the second argument,
+# otherwise returns 1.
+check_version() {
+  local found="${1}"
+  local required="${2}"
 
-# We need check_line_length() from this script cause we want to check if any line has
-# more than 80 characters.
-source ./dev-tools/check_line_length.sh
+  # Use 'sort -V' in order to sort the versions and select the "oldest".
+  local oldest="$(printf '%s\n' "${found}" "${required}" | sort -V | head -n1)"
 
-# Check for existence of clang-format.
-which clang-format >/dev/null
-if [[ $? != 0 ]]; then
-  printf "Failed to find 'clang-format'\n"
-  exit 1
-fi
+  # Ensure the "oldest" of the two versions is what's required.
+  [[ "${oldest}" == "${required}" ]]
+}
 
-# Find all files we want to check in the root directory.
-IFS=:
-file_paths=$(find . -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.proto')
-unset IFS
-
-# Check every file for correct code style.
-check(){
-  if [[ ${#file_paths} == 0 ]]; then
-    printf "There are no files to check!\n"
-    exit 0
-  fi  
-
-  status_exit=0
-
-  for file in ${file_paths}
-  do
-    # Check if the specific file has correct code format.
-    #
-    # If the file is well formatted the command below will return 0. 
-    # We use this fact to print a more helpful message.
-    clang-format-12 --dry-run -Werror --ferror-limit=0  "${file}"
-    format_status=$(echo $?)
-
-    if [[ ${format_status} != 0 ]]; then
-      tput bold # Bold text in terminal.
-      tput setaf 1 # Red font in terminal.
-      printf "${file} file has incorrect code format!\n"
-      tput sgr0
-      tput bold # Bold text in terminal.
-      printf "Command to format ${file}: "
-      tput setaf 2 # Green font in terminal.
-      printf "clang-format --style=Google -i "${file}"\n"
+# Helper that takes a file and ensures it doesn't have lines exceeding
+# 80 characters. Note that we do this check here versus using
+# clang-format because we clang-format to respect programmer added
+# newlines in places where it makes the code easier to read than what
+# clang-format might have done itself.
+check_line_length() {
+  local file="${1}"
+  local -i status=0
+  local -i number=1
+  while IFS= read -r line; do
+    local -i length=${#line}
+    if (( length > 80 )); then
+      status=1
+      tput bold
+      printf "%s:%i:%i: " "$1" "${number}" "${length}"
+      tput setaf 1 # Red.
+      printf "error: "
       tput sgr0 # Reset terminal.
-      exit 1
+      tput bold
+      printf "line exceeds 80 characters\n"
+      tput sgr0 # Reset terminal.
+      printf "%s\n" "${line}"
     fi
+    (( number++ ))
+  done < "${file}"
+  return ${status}
+}
 
-    # Check if any line has more than 80 characters.
-    check_line_length "${file}"
-    status_check_line_length=$(echo $?)
-    if [[ ${status_check_line_length} != 0 ]]; then
-      status_exit=${status_check_line_length}
-    fi
-  done
-  exit ${status_exit}
-} # End of check() function.
+# Check if the specific file has correct code format.
+#
+# If the file is well formatted the command below will return 0.
+# We use this fact to print a more helpful message.
+check_clang_format() {
+  local file="${1}"
 
-check
+  # Check for existence of git.
+  which git >/dev/null
+  if [[ $? != 0 ]]; then
+    printf "Failed to find 'git' (please install or update your path)\n"
+    exit 1
+  fi
+
+  # Check for existence of clang-format.
+  which clang-format >/dev/null
+  if [[ ${?} != 0 ]]; then
+    printf "Failed to find 'clang-format'\n"
+    exit 1
+  fi
+
+  # IMPORTANT NOTE.
+  # The command `clang-format --version` behaves differently on different OS.
+  # For example, on Ubuntu this command will message us out with the following
+  # content:
+  #   Ubuntu clang-format version 12.0.0-3ubuntu1~20.04.4
+  # On macos:
+  #   clang-format version 13.0.0
+  # That's why commands like `cut`, `head`, `tr` are not the way we wanna grab
+  # the version's numbers.
+
+  # Store in `clang_format_version_output` clang-format --version output.
+  clang_format_version_output="$(clang-format --version)"
+
+  # Retain the part after the `version ` (including space too).
+  clang_format_version_found=${clang_format_version_output##*version }
+
+  # Retain the part before `-`.
+  clang_format_version_found=${clang_format_version_found%-*}
+  clang_format_version_required="12.0.0"
+
+  check_version ${clang_format_version_found} ${clang_format_version_required}
+
+  if [[ ${?} != 0 ]]; then
+    printf "clang-format version '%s' is required but found '%s'\n" \
+      "${clang_format_version_required}" \
+      "${clang_format_version_found}"
+    exit 1
+  fi
+
+  # Get top-level directory so we can look for .clang-format file.
+  directory=$(git rev-parse --show-toplevel)
+
+  # Check for .clang-format file.
+  if [[ ! -f "${directory}/.clang-format" ]]; then
+    printf "Failed to find '.clang-format' file at '%s'\n" "${directory}"
+    exit 1
+  fi
+
+  clang-format --dry-run -Werror --ferror-limit=0  "${file}"
+}
+
+check_style() {
+  local file="${1}"
+
+  check_clang_format "${file}" && check_line_length "${file}"
+}
