b55740c120b141e55165c1882ae88cd88aaa8236
CodingCanuck <19922103+CodingCanuck@users.noreply.github.com>
2022-01-12 17:58:08 +0000

    Add a script to measure the flakiness of a command
    Sample output:
    ```bash
    $ ./measure_flakiness.sh "/usr/bin/false"
    Running the following command 100 times to measure flakiness: /usr/bin/false
    0/100 runs succeeded (100% flaky)
    $ ./measure_flakiness.sh "/usr/bin/true"
    Running the following command 100 times to measure flakiness: /usr/bin/true
    100/100 runs succeeded (0% flaky)
    ```

---
+++ measure_flakiness.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Measures the flakiness (the percentage of failures) of the given command.
+#
+# Usage: measure_flakiness.sh $COMMAND_TO_RUN
+
+RUN_COUNT=100
+echo "Running the following command ${RUN_COUNT} times to measure flakiness: ${@}"
+ 
+failure_count=0
+for (( i=0; i<$RUN_COUNT; i++ ))
+do
+    if ! ${@};
+    then
+        failure_count=$((failure_count + 1))
+    fi
+done
+
+success_count=$((RUN_COUNT - failure_count))
+# Round up when dividing so 0.1% flaky is reported as 1%, not 0%. See
+# https://stackoverflow.com/a/2395294
+flaky_percentage=$(((failure_count * 100 + RUN_COUNT - 1) / RUN_COUNT))
+echo "${success_count}/${RUN_COUNT} runs succeeded (${flaky_percentage}% flaky)"
