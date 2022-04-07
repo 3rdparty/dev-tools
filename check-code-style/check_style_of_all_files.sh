76e003b5a0890ddd4f8ec8e53342ae1d871b46f4
archi <arthur_bandaryk@mail.ru>
2022-04-07 17:43:48 +0300

    Exclude all directories with 'submodules' name for code style checks

--- check-code-style/check_style_of_all_files.sh
+++ check-code-style/check_style_of_all_files.sh
@@ -4,9 +4,15 @@ source $(dirname "$0")/check_style.sh
 
 # Check every file for correct code style.
 check_style_of_all_files() {
-  # Find all files we want to check in the workspace.
+  # Find all files we want to check excluding directories with 'submodules' name.
+  #
+  # TODO(Artur): take a list of directories to exclude as part of the action so that
+  # anyone using this can exclude what ever directory they want rather than assume
+  # that everyone using this will want to exclude the 'submodules' directory.
+  # NOTE: do not forget to do the same in the 'check_style_bzl.sh'!!!
+  # https://github.com/3rdparty/dev-tools/blob/main/check-code-style/check_style_bzl.sh 
   IFS=:
-  file_paths=$(find . -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.proto')
+  file_paths=$(find . -type f \( -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' -o -name '*.proto' \) -not -path "*/submodules/*")
   unset IFS
 
   if [[ ${#file_paths} == 0 ]]; then
