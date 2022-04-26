de549b68bde2054e95805f20d94ba5ad18244df2
Bulat Nizamutdinov <Bulat.Nizamutdinov@itechart-group.com>
2022-04-26 10:55:40 +0300

    PR and issue titles as parameters

--- .github/workflows/scripts/check-if-issue-and-pr-exist.sh
+++ .github/workflows/scripts/check-if-issue-and-pr-exist.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PR_TITLE=$1
+ISSUE_TITLE=$2
+
 # The API URLs in conditions below search for any existing pull requests and issues using wildcard and
 # generates ISSUE_EXISTS and PR_EXISTS environment variables and places those in the environment for next steps
 # through $GITHUB_ENV;
@@ -8,11 +11,11 @@
 # If the issue exists we also set the `ISSUE_NUMBER` environment variable.
 
 PR_NUMBER=$(curl -s --request GET \
-  --url https://api.github.com/search/issues\?q=is:pull-request+is:open+repo:$REPO+in:title+Submodule-sync+$MATRIX_PATH+Update+submodule+to+its+latest+version \
-  --header 'Authorization: token ${{ secrets.private_repo_access_as_rebot_token }}' | jq -r ".items[].number" | head -n 1);
+  --url https://api.github.com/search/issues\?q=is:pull-request+is:open+repo:$REPO+in:title+$PR_TITLE \
+  --header "Authorization: token $GITHUB_TOKEN" | jq -r ".items[].number" | head -n 1);
 ISSUE_NUMBER=$(curl -s --request GET \
-  --url https://api.github.com/search/issues\?q=is:issue+is:open+repo:$REPO+in:title+Submodule+sync+failed+for+"$MATRIX_PATH" \
-  --header 'Authorization: token ${{ secrets.private_repo_access_as_rebot_token }}' | jq -r ".items[].number" | head -n 1);
+  --url https://api.github.com/search/issues\?q=is:issue+is:open+repo:$REPO+in:title+$ISSUE_TITLE \
+  --header "Authorization: token $GITHUB_TOKEN" | jq -r ".items[].number" | head -n 1);
 echo "ISSUE_NUMBER=$ISSUE_NUMBER" >> $GITHUB_ENV;
 echo "ISSUE_EXISTS=$(if [[ $ISSUE_NUMBER ]]; then echo 'true'; else echo 'false'; fi)" >> $GITHUB_ENV;
 echo "PR_NUMBER=$PR_NUMBER" >> $GITHUB_ENV;
