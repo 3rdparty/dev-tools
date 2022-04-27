84a24dcd8353d78f583420d1220c563e5841ee7e
Bulat Nizamutdinov <54667918+nizamutdinovb@users.noreply.github.com>
2022-04-27 17:06:24 +0300

    fix path in script (#44)
    Co-authored-by: Bulat Nizamutdinov <Bulat.Nizamutdinov@itechart-group.com>

--- .github/workflows/scripts/per-submodule-build-matrix.sh
+++ .github/workflows/scripts/per-submodule-build-matrix.sh
@@ -7,6 +7,6 @@ git submodule update --recursive --remote
 # The step generates one output: `path_matrix`.
 # `path_matrix` output contains list of all submodules within a repo.
 # The flag is used by the main build job.
-echo "::set-output name=path_matrix::[$(git config --file ../../../.gitmodules --get-regexp path | \
+echo "::set-output name=path_matrix::[$(git config --file ../../../../../.gitmodules --get-regexp path | \
 awk '{ print $2 }' | \
 awk '{ printf "%s\"%s\"", (NR==1?"":", "), $0 } END{ print "" }')]"
