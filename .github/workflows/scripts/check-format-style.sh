02237cb63ae4cf6d65b9fdc0e87dd852bdaa54a1
Bulat Nizamutdinov <Bulat.Nizamutdinov@itechart-group.com>
2022-05-05 20:03:01 +0300

    Introduce Python format check

---
+++ .github/workflows/scripts/check-format-style.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+YAPF_COMMAND=$(yapf -p --diff --recursive .)
+if [[ "${YAPF_COMMAND}" ]]; then
+  echo "Please fix Python files format using a command 'yapf -p -i --recursive .'"
+  echo "${YAPF_COMMAND}"
+  exit 1
+fi
