0e316b08fc392fbe1efa686d7e1b5b3dc7b491e0
archi <arthur_bandaryk@mail.ru>
2022-05-17 14:34:07 +0300

    Minor code changes and adding comments

--- .github/workflows/scripts/update-eventuals-git-repository.sh
+++ .github/workflows/scripts/update-eventuals-git-repository.sh
@@ -1,40 +1,70 @@
 # !/bin/bash
 
+# File passed to this script as an argument in which we gonna
+# update 'git_repository' bazel rule for eventuals 
+# (WORKSPACE.bazel usually).
 file=$1
 
-commit=$(git log -n 1 --pretty=format:"%H")
-shallow_since=$(git log -n 1 --date=raw --pretty=format:"%cd")
+# The last commit for eventuals.
+commit=$2
 
+# The latest shallow_since for eventuals.
+shallow_since=$3
+
+if [[ ! -f "$file" ]]; then
+    echo "$file doesn't exist"
+    exit 1
+fi
+
+if [[ ${#commit} == 0 ]]; then
+    echo "Incorrect commit value."
+    exit 1
+fi
+
+if [[ ${#shallow_since} == 0 ]]; then
+    echo "Incorrect shallow_since value."
+    exit 1
+fi
+
+# This flag indicates whether or not we process correct
+# 'git_repository' rule for eventuals in current $file.
 git_repository_needed=False
+
+# Flags to help understand whether or not all updates
+# (commit and shallow_since) are done.
+is_commit_replaced=False
+is_shallow_replaced=False
 
 while IFS= read -r line
 do
+  # Check if current line of the file contains necessary name
+  # of the git_repository which we gonna update with commit
+  # and shallow_since.
   if [[ "${line}" =~ .*"name = \"com_github_3rdparty_eventuals\"".* ]];
   then
     git_repository_needed=True
-    echo "line with name of git_repository needed: ${line}"
   fi
 
-  if [[ "${line}" =~ .*"commit = ".* ]];
+  # This is a right place for commit updates.
+  if [[ "${line}" =~ .*"commit = ".* && $git_repository_needed = True ]];
   then
-    if [ $git_repository_needed = True ];
-    then
-      new_commit_replace="commit = \"${commit}\","
-      sed -i "s/$line/$new_commit_replace/" $file
-      echo $new_commit_replace
-    fi    
+    new_commit_replace="commit = \"${commit}\","
+    sed -i "s/$line/$new_commit_replace/" $file
+    is_commit_replaced=True
   fi
 
-  if [[ "${line}" =~ .*"shallow_since = ".* ]];
+  # This is a right place for shallow_since updates.
+  if [[ "${line}" =~ .*"shallow_since = ".* && $git_repository_needed = True ]];
   then
-    if [ $git_repository_needed = True ];
-    then
-      new_shallow_replace="shallow_since = \"${shallow_since}\","
-      sed -i "s/$line/$new_shallow_replace/" $file
-      echo $new_shallow_replace
-    fi    
+    new_shallow_replace="shallow_since = \"${shallow_since}\","
+    sed -i "s/$line/$new_shallow_replace/" $file
+    is_shallow_replaced=True
   fi
 
+  if [[ $is_commit_replaced = True && $is_shallow_replaced = True ]];
+  then
+    break
+  fi
 done < "$file"
 
 # Check for existence of buildifier.
