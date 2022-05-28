0cd3a0de6d5b45fe74ae28c2a636dfd779cb5340
Stephanie <stephanie@reboot.dev>
2022-05-27 18:55:55 -0700

    respond to comments

--- check-code-style/check_style.sh
+++ check-code-style/check_style.sh
@@ -30,14 +30,14 @@ check_line_length() {
   local file="${1}"
   local -i status=0
   local -i number=1
-  local -i skip_next_line=0
+  local skip_next_line=false
   while IFS= read -r line; do
-    if (( skip_next_line )); then
-      (( skip_next_line-- ))
+    if [[ $skip_next_line == true ]]; then
+      skip_next_line=false
       continue
     fi
     if [[ $line == *"check_line_length skip"* ]]; then
-      skip_next_line=1
+      skip_next_line=true
     fi
     local -i length=${#line}
     if (( length > 80 )); then
